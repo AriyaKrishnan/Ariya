99616334f07441cd16c7818bfbd026d307b7938b
ariya.ramakrishnan <ariya.ramakrishnan@DT150158.QICHOAPPL.com>
2022-05-05 12:46:38 +0530

    5May

--- Project/src/main/java/HelloWorld.java
+++ Project/src/main/java/HelloWorld.java
@@ -1,10 +1,17 @@
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class HelloWorld {
-
+	static Logger logger=Logger.getLogger(HelloWorld.class);
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-System.out.println("Hello");
-		System.out.println("World");
+		String str="welcome to sonar";
+		String log4jConfigFile = System.getProperty("user.dir") + File.separator + "log4j.properties";
+		PropertyConfigurator.configure(log4jConfigFile);
+		logger.info("this is a message....:"+str);
+		logger.warn("warning message...:"+str);
+		logger.error("error message...:"+str);
 	}
 
 }
---
+++ sonar/src/main/java/sonar/HelloWorld.java
@@ -0,0 +1,10 @@
+package sonar;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
